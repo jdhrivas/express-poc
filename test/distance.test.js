b6eb9205b89e656afab98104501afac9645922a0
David Hernandez <david.hernandez@azulcorporation.com>
2019-05-25 23:27:02 -0500

    fix mocha

--- test/distance.test.js
+++ test/distance.test.js
@@ -6,10 +6,6 @@ describe ('GET /about', function(){
     it('expects HTTP response 200', function(done){
         request(app)
         .get('/about')
-        .expect(200)
-        .end((err) => {
-            if (err) return done(err);
-            done();
-        });
+        .expect(200, done)
     })
 })
