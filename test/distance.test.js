e3db6d1e5cdcd1b5c0c13b9e2ecfe1bdf607fa09
David Hernandez <david.hernandez@azulcorporation.com>
2019-05-25 21:01:58 -0500

    first commit

---
+++ test/distance.test.js
@@ -0,0 +1,11 @@
+'use strict'
+var request = require('supertest')
+var app = require('../server')
+
+describe ('GET /about', function(){
+    it('expects HTTP response 200', function(done){
+        request(app)
+        .get('/about')
+        .expect(200, done)
+    })
+})
